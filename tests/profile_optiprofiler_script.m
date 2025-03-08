4edd871802b7c7ff08da039a481f80759015be92
Lht97 <lhtian97@gmail.com>
2025-03-08 17:11:48 +0800

    Since the place of artifact got by optiprofiler has been moved to other destination, change something in .gitignore

--- tests/profile_optiprofiler_script.m
+++ tests/profile_optiprofiler_script.m
@@ -1,18 +1,20 @@
 clear all
 % options.dim = 'big';
-options.solver_names = {'cbds', 'nomad'};
+% options.solver_names = {'cbds', 'nomad'};
+options.solver_names = {'cbds', 'newuoa'};
 % feature = {'perturbed_x0_0.001', 'perturbed_x0_1', 'perturbed_x0_10', 'perturbed_x0_100', 'truncated_1', 'truncated_2', 'truncated_3', 'truncated_4', 'permuted', 'random_nan_5', 'random_nan_10', 'random_nan_20'};
 % for i = 1:length(feature)
 %     options.feature_name = feature{i};
 %     profile_optiprofiler(options);
 % end
-options.dim = 'big';
+options.dim = 'small';
+options.load = '20250308_165004';
 %options.solver_names = {'rbds-zero-delay', 'rbds-one-delay', 'rbds-eighth-delay', 'rbds-quarter-delay', 'rbds-half-delay', 'rbds-n-minus-one-delay'};
 options.feature_name = 'plain';
 profile_optiprofiler(options);
 
-options.feature_name = 'linearly_transformed';
-profile_optiprofiler(options);
+% options.feature_name = 'linearly_transformed';
+% profile_optiprofiler(options);
 % options.feature_name = 'perturbed_x0_1';
 % profile_optiprofiler(options);
 
@@ -22,8 +24,8 @@ profile_optiprofiler(options);
 % options.feature_name = 'noisy_1e-2';
 % profile_optiprofiler(options);
 
-options.feature_name = 'noisy_1e-3';
-profile_optiprofiler(options);
+% options.feature_name = 'noisy_1e-3';
+% profile_optiprofiler(options);
 
 % options.feature_name = 'noisy_1e-4';
 % profile_optiprofiler(options);
