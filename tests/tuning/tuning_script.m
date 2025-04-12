0c2b045adbc00ee3f3fbd9fdeab72cd921340243
Lht97 <lhtian97@gmail.com>
2025-04-12 11:00:13 +0800

    change the tuning_script.m

--- tests/tuning/tuning_script.m
+++ tests/tuning/tuning_script.m
@@ -5,14 +5,14 @@ parameters.grad_window_size = [3 5 6 8 10 12 15];
 % parameters.func_tol = 10.^([-15 -12 -10 -8 -6 -5 -4 -3]);
 % parameters.func_tol_ratio = 1e-3;
 parameters.grad_tol = 10.^([-15 -12 -10 -8 -6 -5 -3]);
-parameters.grad_tol_ratio = 1e-3;
+parameters.grad_tol_ratio = 1e-2;
 % parameters.orthogonal_directions = true;
 % parameters.grad_tol_1 = 10.^(-4:-2:-14);
 % parameters.grad_tol_2 = 10.^(-4:-2:-14);
 % parameters.expand = 1.2:0.1:1.8;
 % parameters.shrink = 0.5:0.1:0.7;
 options.mindim = 1;
-options.maxdim = 1;
+options.maxdim = 5;
 if ~isfield(options, 'n_runs')
     options.n_runs = 1;
 end
@@ -39,23 +39,23 @@ options.feature_name = 'plain';
 fprintf('Feature:\t %s\n', options.feature_name);
 tuning_script_optiprofiler(parameters, options);
 
-% options.feature_name = 'linearly_transformed';
-% fprintf('Feature:\t %s\n', options.feature_name);
-% tuning_script_optiprofiler(parameters, options);
+options.feature_name = 'linearly_transformed';
+fprintf('Feature:\t %s\n', options.feature_name);
+tuning_script_optiprofiler(parameters, options);
 
-% options.tau_weights(1, 1:options.max_tol_order, 2, 1) = [0.9/(options.max_tol_order-4)*ones(1, options.max_tol_order-4), 0.025, 0.025, 0.025, 0.025]; 
-% if sum(options.tau_weights(:)) ~= 1
-%     error('Sum of tau_weights must be 1');
-% end
-% % Check if max_tol_order is equal to the length of tau_weights.
-% if  (options.max_tol_order ~= size(options.tau_weights, 2))
-%     error('max_tol_order must be equal to the length of tau_weights');
-% end
-% 
-% options.feature_name = 'noisy_1e-3';
-% fprintf('Feature:\t %s\n', options.feature_name);
-% tuning_script_optiprofiler(parameters, options);
-% 
+options.tau_weights(1, 1:options.max_tol_order, 2, 1) = [0.9/(options.max_tol_order-4)*ones(1, options.max_tol_order-4), 0.025, 0.025, 0.025, 0.025]; 
+if sum(options.tau_weights(:)) ~= 1
+    error('Sum of tau_weights must be 1');
+end
+% Check if max_tol_order is equal to the length of tau_weights.
+if  (options.max_tol_order ~= size(options.tau_weights, 2))
+    error('max_tol_order must be equal to the length of tau_weights');
+end
+
+options.feature_name = 'noisy_1e-3';
+fprintf('Feature:\t %s\n', options.feature_name);
+tuning_script_optiprofiler(parameters, options);
+
 % options.feature_name = 'rotation_noisy_1e-3';
 % fprintf('Feature:\t %s\n', options.feature_name);
 % tuning_script_optiprofiler(parameters, options);
