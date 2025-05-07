b239bd44e648f8d9e497a40fd75bc090613e81ac
Lht97 <lhtian97@gmail.com>
2025-05-08 00:36:40 +0800

    change the setting of profile_optiprofiler_script

--- tests/profile_optiprofiler_script.m
+++ tests/profile_optiprofiler_script.m
@@ -1,17 +1,25 @@
 clear all
 % options.dim = 'big';
-% options.solver_names = {'cbds', 'nomad'};
 options.solver_names = {'fd-bfgs', 'praxis'};
-% feature = {'perturbed_x0_0.001', 'perturbed_x0_1', 'perturbed_x0_10', 'perturbed_x0_100', 'truncated_1', 'truncated_2', 'truncated_3', 'truncated_4', 'permuted', 'random_nan_5', 'random_nan_10', 'random_nan_20'};
-% for i = 1:length(feature)
-%     options.feature_name = feature{i};
-%     profile_optiprofiler(options);
-% end
 options.mindim = 1;
-options.maxdim = 1;
-%options.solver_names = {'rbds-zero-delay', 'rbds-one-delay', 'rbds-eighth-delay', 'rbds-quarter-delay', 'rbds-half-delay', 'rbds-n-minus-one-delay'};
+options.maxdim = 5;
 options.feature_name = 'plain';
 profile_optiprofiler(options);
+
+options.feature_name = 'noisy_1e-3';
+profile_optiprofiler(options);
+
+options.feature_name = 'noisy_1e-6';
+profile_optiprofiler(options);
+
+options.solver_names = {'default-fd-bfgs', 'praxis'};
+
+options.feature_name = 'noisy_1e-3';
+profile_optiprofiler(options);
+
+options.feature_name = 'noisy_1e-6';
+profile_optiprofiler(options);
+
 
 % options.feature_name = 'linearly_transformed';
 % profile_optiprofiler(options);
