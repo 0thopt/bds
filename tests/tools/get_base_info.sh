7f46e1d44cc16e09db9b84772ae9a76650325093
Lht97 <lhtian97@gmail.com>
2025-05-26 00:10:26 +0800

    get_base_info should also return the dimensions, small or big

--- tests/tools/get_base_info.sh
+++ tests/tools/get_base_info.sh
@@ -1,2 +1,2 @@
 ARTIFACT_NAME=$(ls | grep "profile_optiprofiler" | head -n 1)
-BASE_INFO=$(echo $ARTIFACT_NAME | sed -n 's/.*optiprofiler_\(.*\)_\(small\|big\).*/\1/p')
+BASE_INFO=$(echo $ARTIFACT_NAME | sed -n 's/.*optiprofiler_\(.*\)_\(small\|big\).*/\1_\2/p')
