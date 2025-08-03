74bf449f50f563be3767788d2fa56f44016b6762
Lht97 <lhtian97@gmail.com>
2025-08-03 16:58:28 +0800

    update the profile_optiprofiler_script.m

--- tests/profile_optiprofiler_script.m
+++ tests/profile_optiprofiler_script.m
@@ -1,43 +1,16 @@
 clear all
 options.dim = 'big';
 options.plibs = 'matcutest';
-options.solver_names = {'our-method', 'ds'};
-
-options.feature_name = 'plain';
-profile_optiprofiler(options);
+options.feature_name = 'linearly_transformed';
 
 options.solver_names = {'cbds', 'ds'};
 
-options.feature_name = 'plain';
-profile_optiprofiler(options);
-
-options.feature_name = 'noisy_1e-3';
-profile_optiprofiler(options);
-
-options.feature_name = 'rotation_noisy_1e-3';
 profile_optiprofiler(options);
 
 options.solver_names = {'cbds', 'newuoa'};
 
-options.feature_name = 'plain';
-profile_optiprofiler(options);
-
-options.feature_name = 'noisy_1e-3';
-profile_optiprofiler(options);
-
-options.feature_name = 'rotation_noisy_1e-3';
 profile_optiprofiler(options);
 
 options.solver_names = {'cbds', 'fd-bfgs'};
 
-options.feature_name = 'plain';
 profile_optiprofiler(options);
-
-options.feature_name = 'noisy_1e-3';
-profile_optiprofiler(options);
-
-options.feature_name = 'rotation_noisy_1e-3';
-profile_optiprofiler(options);
-
-
-
