83527f19499b429956dcfd2dd1d509e336ef405f
Lht97 <lhtian97@gmail.com>
2026-01-25 12:38:55 +0800

    change get_base_info.sh to make it more strict

--- tests/tools/get_base_info.sh
+++ tests/tools/get_base_info.sh
@@ -1,2 +1,2 @@
 ARTIFACT_NAME=$(ls | grep "profile_optiprofiler" | head -n 1)
-BASE_INFO=$(echo $ARTIFACT_NAME | sed -n 's/.*optiprofiler_\([^_]*_[^_]*_\(small\|big\|large\)[^_]*\).*/\1/p')
+BASE_INFO=$(echo $ARTIFACT_NAME | sed -n 's/.*optiprofiler_\([^_]*_[^_]*_[^_]*_\(small\|big\|large\)\).*/\1/p')
