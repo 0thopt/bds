33f88ff333df3b30f4a6faf30177a738b1910f03
Lht97 <lhtian97@gmail.com>
2026-03-10 14:44:33 +0800

    change the example of cycling.m

--- src/private/eval_fun.m
+++ src/private/eval_fun.m
@@ -55,32 +55,6 @@ end
 % % See 4.5 of "PDFO: A Cross-Platform Package for Powell's Derivative-Free Optimization Solvers" 
 % % by Tom M. Ragonneau and Zaikun Zhang.
 % if isnan(f_real)
-%     f_real = inf;
-% end
-% f = min([f_real, 1e30, sqrt(realmax())]);
-
-% end
-
-% function [f, f_real, is_valid] = eval_fun(fun, x)
-% %EVAL_FUN evaluates function FUN at point X, returning f and f_real.
-% %   f_real is the real function value, while f is a moderated version of f_real.
-% %   The moderation is to handle nan, huge values, and evaluation failures. The 
-% %   algorithm will operate on f, while f_real is used for recording the history.
-
-% is_valid = true;
-
-% try
-%     f_real = fun(x);
-% catch
-%     warning('The function evaluation failed.');
-%     f_real = nan;
-%     is_valid = false;
-% end
-
-% % Apply the moderate extreme barrier to handle nan, huge values, and evaluation failures.
-% % See 4.5 of "PDFO: A Cross-Platform Package for Powell's Derivative-Free Optimization Solvers" 
-% % by Tom M. Ragonneau and Zaikun Zhang.
-% if isnan(f_real)
 %     f = 1e30;
 % else
 %     f = f_real;
