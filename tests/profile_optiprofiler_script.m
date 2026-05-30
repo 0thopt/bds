793d2bc3ec88d387dd53ca312096c34e5f79d9a6
Lht97 <lhtian97@gmail.com>
2026-05-30 16:27:04 +0800

    save

--- tests/profile_optiprofiler_script.m
+++ tests/profile_optiprofiler_script.m
@@ -7,4 +7,7 @@ options.feature_name = 'plain';
 options.solver_names = {'bds', 'ds', 'nelder-mead', 'bfo', 'pds', 'nomad'};
 options.solver_names = {'BDS-scaled', 'BDS-default'};
 options.solver_names = {'bds', 'bds-simplified'};
+options.feature_name = 'random_nan_1';
+profile_optiprofiler(options);
+options.feature_name = 'random_nan_5';
 profile_optiprofiler(options);
